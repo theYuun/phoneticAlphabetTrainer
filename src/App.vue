3cf6c897f424d00ef347c2068eb870d536b6a1a8
Theuins Lombard <theunis.lombard@kidsindustries.com>
2024-02-16 18:59:38 +0200

    *functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,111 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from 'vue';
+import data from './assets/data';
+
+const minMax = [1, 3]; //[3, 7];
+const combo = () => {
+  let question = '';
+  for(let i = 0; i < minMax[0] + Math.floor(Math.random() * minMax[1]); i++)
+  {
+    /*
+    */
+    //console.log('alphabet key: ', Object.keys(data.alphabet)[Math.floor(Math.random() * Object.keys(data.alphabet).length)]);
+    const main = Object.keys(Math.random() < 0.7 ? data.alphabet : data.numbers);
+    //console.log('main: ', main);
+    const sub = main[Math.floor(Math.random() * main.length)];
+    //const sub = Object.keys(data.numbers)[Math.floor(Math.random() * Object.keys(data.numbers).length)];
+    //console.log('sub: ', sub);
+    question += sub.toUpperCase();
+  }
+  //console.log(question);
+  return question;
+}
+const selectedSequence = ref(combo());
+function GetNewSequence() {
+  answered.value = false;
+  selectedSequence.value = combo();
+  //console.log('new combo: ', selectedSequence.value);
+}
+
+const input = ref('');
+const isCorrect = ref(true);
+const answered = ref(false);
+function CheckAnswer() {
+  answered.value = true;
+  const answer = input.value.split(' ');
+  const question = selectedSequence.value.split('');
+  const incorrectValues = ref([]);
+  for (let i =  0; i < question.length; i++) {
+    //console.log('question: ', question[i], ' input: ', input.value, ' answer: ', answer[i], ' answer array: ', answer, ' reference: ', Object.values(data.numbers[question[i]]).join(''))
+    if(!isNaN(Number(question[i]))) {
+      console.log('answer: ', answer[i], ' reference: ', Object.values(data.numbers[question[i]]).join(''))
+      if(answer[i] !== Object.values(data.numbers[question[i]]).join('')) {
+        incorrectValues.value.push(i);
+        console.log('wrong number: ', incorrectValues.value)
+      }
+    }
+    else {
+      console.log('answer: ', answer[i])
+      console.log(' 0: ', Object.values(data))
+      console.log(' 1: ', Object.values(data.alphabet))
+      console.log(' 2: ', question[i])
+      console.log(' reference: ', Object.values(data.alphabet[question[i].toLowerCase()]).join(''))
+      console.log(' 4: ', data.alphabet[question[i].toLowerCase()])
+      if(answer[i] !== Object.values(data.alphabet[question[i].toLowerCase()]).join(''))
+        incorrectValues.value.push(i)
+        console.log('wrong letter: ', incorrectValues.value)
+    }
+  }
+  // error about is that the match is happening on only the first letter of the data.numbers/data.alphabet values
+  /*
+  /*
+  for (let i = 0; i < question.length; i++) {
+    if(typeof question[i] === 'string' && !isNaN(question[i])) {
+      console.log('answer: ', answer[i], ' reference: ', Object.entries(data.numbers[question[i]]))
+      
+      if(answer[i] !== data.numbers[question[i]]) {
+        incorrectValues.value.push(i);
+        console.log(i);
+      }
+      
+    }
+    else if(typeof question[i] === 'string' && isNaN(question[i])) {
+      console.log('answer: ', answer[i], ' reference: ', Object.entries(data.alphabet[question[i]]))
+      
+      if(answer[i] !== data.alphabet[question[i]]) {
+        incorrectValues.value.push(i);
+        console.log(i);
+      }
+      
+    }
+  }
+  */
+  isCorrect.value = incorrectValues.value.length > 0 ? false : true;
+  console.log(isCorrect.value);
+}
+
 </script>
 
 <template>
+  <!--
+    Add function to randomly select a letter from the phonetic alphabet and prompts the correct reference
+    Add another function to display a letter and number combo with prompt to fill in entire correct sequence
+  -->
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <span>{{ selectedSequence }}</span>
+    <!--
+
+      @input="console.log(input)"
+    -->
+    <input
+      v-model="input"
+      placeholder="phonetic word"/>
+    <input type="button" value="check answer" @click="CheckAnswer()" />
+    <input type="button" value="select new combo" @click="GetNewSequence()" />
+    <span v-if="answered && !isCorrect">Check values {{ incorrectValues }}</span>
+    <span v-if="answered && isCorrect">Well done!</span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
 </style>
---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+const data = {
+    alphabet: {
+        a: 'alpha',
+        b: 'bravo',
+        c: 'charlie',
+        d: 'delta',
+        e: 'echo',
+        f: 'foxtrot',
+        g: 'golf',
+        h: 'hotel',
+        i: 'india',
+        j: 'juliet',
+        k: 'kilo',
+        m: 'mike',
+        n: 'november',
+        o: 'oscar',
+        p: 'papa',
+        q: 'quebec',
+        r: 'romeo',
+        s: 'sierra',
+        t: 'tango',
+        u: 'uniform',
+        v: 'victor',
+        w: 'whiskey',
+        x: 'x-ray',
+        y: 'yankee',
+        z: 'zulu',
+    },
+    numbers: {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        0: 'zero',
+    }
+}
+
+export default data;
